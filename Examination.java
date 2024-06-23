b3f497746dcdd8162916a76dadfff1a983a6138a
sammy-sea <113259386+sammy-sea@users.noreply.github.com>
2024-06-22 18:46:42 -0700

    Update Examination.java

--- Examination.java
+++ Examination.java
@@ -6,14 +6,12 @@ import java.util.Date;
 public class  Examination {
     private String patientId;
     private Date date;
-    private ArrayList<Note> doctorNotes;
-    private ArrayList<Note> nurseNotes;
+
 
     public Examination(String patientId, Date date, ArrayList<Note> doctorNotes, ArrayList<Note> nurseNotes) {
         this.patientId = patientId;
         this.date = date;
-        this.doctorNotes = doctorNotes;
-        this.nurseNotes = nurseNotes;
+
     }
   //opperations
 
