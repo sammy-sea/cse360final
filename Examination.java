6e51e35479e38b4019bf955019b0db2ae5f11cf4
sammy-sea <113259386+sammy-sea@users.noreply.github.com>
2024-06-22 18:03:28 -0700

    Update Examination.java

--- Examination.java
+++ Examination.java
@@ -1,1 +1,35 @@
+package com.example.cse360project;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+public class  Examination {
+    private String patientId;
+    private Date date;
+    private ArrayList<Note> doctorNotes;
+    private ArrayList<Note> nurseNotes;
+
+    public Examination(String patientId, Date date, ArrayList<Note> doctorNotes, ArrayList<Note> nurseNotes) {
+        this.patientId = patientId;
+        this.date = date;
+        this.doctorNotes = doctorNotes;
+        this.nurseNotes = nurseNotes;
+    }
+  //opperations
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public ArrayList<Note> getDoctorNotes() {
+        return doctorNotes;
+    }
+
+    public ArrayList<Note> getNurseNotes() {
+        return nurseNotes;
+    }
+}
