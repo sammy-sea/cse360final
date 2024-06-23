6200a285070aa01a8bb4f1a1faa526e511f7cc37
sammy-sea <113259386+sammy-sea@users.noreply.github.com>
2024-06-22 18:01:05 -0700

    Create Vitals.java

---
+++ Vitals.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+
+public class Vitals {
+    private Date date;
+    private String patientId;
+    private String weight;
+    private String height;
+    private int bodyTemperature;
+    private int bloodPressure;
+
+    public Vitals(Date date, String patientId, String weight, String height, int bodyTemperature, int bloodPressure) {
+        this.date = date;
+        this.patientId = patientId;
+        this.weight = weight;
+        this.height = height;
+        this.bodyTemperature = bodyTemperature;
+        this.bloodPressure = bloodPressure;
+    }
+  //opperations
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public int getBodyTemperature() {
+        return bodyTemperature;
+    }
+
+    public int getBloodPressure() {
+        return bloodPressure;
+    }
+}
